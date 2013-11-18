8890295c78c491d9d5ec131c1184c99eb9a151bf
Daniel Mills <mills.dma@gmail.com>
2013-11-17 22:02:25 -0500

    implemented command arguments

--- IrcBot.js
+++ IrcBot.js
@@ -1,97 +1,120 @@
-var net = require('net');//,
-	//Command = require('./command');
+var net = require('net');
 
 /* Expose IrcBot */
 module.exports = IrcBot;
 
-/*
-	options:
-		host
-		port
-		nick
-		name
-		channel list
-		commands?
-*/
+
 function IrcBot(options) {
-	var self = this;
-	self.opt = options;
-	self.conn = null;
-	self.nameRegex = new RegExp(':!'+self.opt.nick);
+    var self = this;
+    self.opt = options;
+    self.conn = null;
+    self.nameRegex = new RegExp(self.opt.nick);
 }
 
 IrcBot.prototype.onData = function(data) {
-	console.log(data);
+    console.log(data);
 
-	//Response of successful connection
-	if(/001/.test(data)) {
-		this.joinChannels();
-	}
+    //Get hostname 
+    if(!this.opt.hostname) {
+        this.opt.hostname = data.split(' ')[0].substr(1);
+        console.log('stored hostname:' + this.opt.hostname);
+    }
 
-	//REPLY TO PINGS
-	if(/PING/.test(data)) {
-		this.sendMessage('PONG ' + data.substr(5));
-	}
+    //Response of successful connection
+    if(/001/.test(data)) {
+        this.joinChannels();
+    }
 
-	//SOMEONE CALLING MAH NAME
-	if(/PRIVMSG/.test(data) && this.nameRegex.test(data)) {
-		var cut = data.indexOf('PRIVMSG')+36;
-		this.parseMessage(data.substr(cut));
-	}
+    //REPLY TO PINGS
+    if(/PING/.test(data)) {
+        this.sendMessage('PONG ' + this.opt.hostname);
+    }
+
+    //Our name has been said
+    if(/PRIVMSG/.test(data) && this.nameRegex.test(data)) {
+
+        //remove \r\n and split
+        var splitData = data.trimRight(4).split(' '),
+            messageArgs = this.generateMessageArgs(splitData, (splitData[2][0] !== '#'));
+
+        this.parseMessage(messageArgs);
+    }
 };
 
-IrcBot.prototype.onConnect = function(data) {
-	this.sendMessage('USER ' + this.opt.nick + ' testbot testbot :testbot123');
-	this.sendMessage('NICK ' + this.opt.nick);
+IrcBot.prototype.generateMessageArgs = function(dataArr,isPrivate) {
+
+    var speaker = dataArr[0].substr(1,dataArr[0].indexOf('!')-1);
+
+    var messageArgs = {
+        isPrivate:isPrivate,
+        rawstring:dataArr.join(' '),
+        hostname:dataArr[0],
+        speakername:speaker
+    };
+    
+    if(!isPrivate) {
+        messageArgs.roomname = dataArr[2];
+    }
+
+    if((isPrivate && dataArr.length > 4) || (!isPrivate && dataArr.length > 5))
+        messageArgs.args = (isPrivate) ? dataArr.slice(3).join(' '): dataArr.slice(5).join(' ');
+
+    messageArgs.botCommand = (isPrivate)? dataArr[3] : dataArr[4];
+
+    return messageArgs;
+};
+
+IrcBot.prototype.onConnect = function() {
+
+    this.sendMessage('USER ' + this.opt.nick + ' testbot testbot :testbot123');
+    this.sendMessage('NICK ' + this.opt.nick);
 };
 
 /*
-	Called when bot's name is mentioned in a PRIVMSG
-	parse out commands and executes them.
+    Called when bot's name is mentioned in a PRIVMSG
+    parse out commands and executes them.
 */
-IrcBot.prototype.parseMessage = function(message) {
-
-	console.log('message: '+message);
-	var commands = this.opt.commands;
-
-	for(var i=0;i<commands.length;i++) {
-		if(commands[i].test(message)) {
-			var results = commands[i].action(message);
-			for(var j=0;j<results.length;j++) {
-				this.sendMessage(results[j]);
-			}
-			break;
-		}
-	}	
+IrcBot.prototype.parseMessage = function(args) {
+    var commands = this.opt.commands;
+    console.log('message: '+JSON.stringify(args));
+    for(var i=0;i<commands.length;i++) {
+        if(commands[i].test(args.rawstring)) {
+            var results = commands[i].action(args);
+            for(var j=0;j<results.length;j++) {
+                this.sendMessage(results[j]);
+            }
+            break;
+        }
+    }   
 };
 
 /*
-	Called after connection is successful
-	joins all channels passed as options
+    Called after connection is successful
+    joins all channels passed as options
 */
 IrcBot.prototype.joinChannels = function() {
-	var channels = this.opt.channels,
-		i;
-	for(i=0; i<channels.length;i++) {
-		this.sendMessage('JOIN '+channels[i]);
-	}
+    var channels = this.opt.channels,
+        i;
+    for(i=0; i<channels.length;i++) {
+        this.sendMessage('JOIN '+channels[i]);
+    }
 };
 
 IrcBot.prototype.sendMessage = function(message) {
-	this.conn.write(message+'\r\n');
+    this.conn.write(message+'\r\n');
 };
 
 IrcBot.prototype.connect = function() {
-	var self = this;
+    var self = this;
 
-	self.conn = net.connect({
-		host:self.opt.host,
-		port:self.opt.port
-	},function() {
-		console.log('Connected.');
-	});
-	self.conn.setEncoding('utf-8');
+    self.conn = net.connect({
+        host:self.opt.host,
+        port:self.opt.port
+    },function(data) {
+        console.log('Connected.');
+    });
+    self.conn.setEncoding('utf-8');
 
-	self.conn.on('data',self.onData.bind(self));
-	self.conn.on('connect',self.onConnect.bind(self));
+    self.conn.on('data',self.onData.bind(self));
+    self.conn.on('connect',self.onConnect.bind(self));
 };
--- command.js
+++ command.js
@@ -1,8 +1,18 @@
 module.exports = Command;
 
-function Command(regex,action) {
+function Command(name,description,regex,action) {
+	this.name = name;
+	this.description = description;
 	this.regex = regex;
 	this.action	= action;
+}
+
+Command.prototype.name = function() {
+	return this.name;
+};
+
+Command.prototype.description = function() {
+	return this.description;
 };
 
 Command.prototype.test = function(str) {
